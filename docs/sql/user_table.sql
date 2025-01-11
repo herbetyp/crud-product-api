65ddd370497ce62752120848a2d29844b024c30b
herbetyp <herbetypaulo@gmail.com>
2025-01-10 21:07:55 -0300

    doc: users table

--- docs/sql/user_table.sql
+++ docs/sql/user_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE users (
 	username VARCHAR(100) NOT NULL,
 	email VARCHAR(100) NOT NULL,
 	password VARCHAR(255) NOT NULL,
+	uid UUID DEFAULT gen_random_uuid (),
 	created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
 	updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
 	UNIQUE (email)
